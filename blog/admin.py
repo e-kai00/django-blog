1fc92b309645f3f60f14d0a0f26a1c6152262151
Katerina <yelkaterina@gmail.com>
2023-06-04 17:11:32 +0200

    Render homepage

--- blog/admin.py
+++ blog/admin.py
@@ -1,27 +1,24 @@
 from django.contrib import admin
 from .models import Post, Comment
-from django_summernote.admin import SummernoteModelAdmin
+# from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Post)
-class PostAdmin(SummernoteModelAdmin):
+class PostAdmin(admin.ModelAdmin):
 
     list_display = ('title', 'slug', 'status', 'created_on')
     search_fields = ['title', 'content']
-    prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'created_on')
-    summernote_fields = ('content')
-
+    prepopulated_fields = {'slug': ('title',)}
+    # summernote_fields = ('content',)
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-
     list_display = ('name', 'body', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
-    search_fields = ['name', 'email', 'body']    
+    search_fields = ('name', 'email', 'body')
     actions = ['approve_comments']
 
     def approve_comments(self, request, queryset):
-        queryset.update(approved=True) 
-
+        queryset.update(approved=True)
--- codestar/settings.py
+++ codestar/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
-    'django_summernote',
+    # 'django_summernote',
     'blog',
 ]
 
